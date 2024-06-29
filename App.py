657e550aac43b94666d6e1c3478252deb0b4e07e
Art Lisboa <lisboamillen30@gmail.com>
2024-06-29 15:59:55 +0800

    added serial com

--- App.py
+++ App.py
@@ -1,40 +1,34 @@
 from flask import Flask, render_template, Response,jsonify
 from model.Object_detection import ObjectDetector
-from firebase.Firebase import Firebase
+
+from firebase.ArduinoCom import Serial
+
 import serial
 import time
-from flask_serial import Serial
 import cv2
 
 
 
 app = Flask(__name__)
 
-ser = None
-
-def data_serial():
-    global ser
-    ser = serial.Serial('COM7', 9600)
-    ser.timeout = 1
+app.config["SERIAL_TIMEOUT"] = 1
+app.config["SERIAL_PORT"] = "COM7"
+app.config["SERIAL_BAUDRATE"] = 9600
     
+
 
 # Create an instance of the ObjectDetector class
 object_detector = ObjectDetector(model='model/ssd_mobilenet_v2.tflite')
 object_detector.run()
+
+ser = Serial(app)
 
 # serial comunication
 @app.route('/serial_IR', methods=['GET'])
 def serial_IR():
     
     try:
-        data = data_serial()
-        # Define the serial port and baud rate
-        ser.reset_input_buffer()
-
-        ser.flush()
-        data = ser.readline().decode('utf-8').rstrip()
-        print(data)
-        return jsonify(data)
+        return jsonify("wait")
     except:
         return jsonify("0,0")
 
@@ -72,7 +66,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    ser = serial.Serial('COM7', 9600)
+
     app.run(
         host='0.0.0.0',
         debug=True,
--- firebase/ArduinoCom.py
+++ firebase/ArduinoCom.py
@@ -4,21 +4,27 @@ import threading
 import time
 
 class Serial:
-    def __init__(self,app):
+    def __init__(self,app=None):
         self.serial = serial
         self.Ser = None
+        
+        self.timeout  = 1
+        self.port     = "COM7"
+        self.baudrate = 9600
+        
+        self.message = "nag reset"
 
         if app is not None:
             self.timeout  = app.config.get("SERIAL_TIMEOUT")
             self.port     = app.config.get("SERIAL_PORT")
             self.baudrate = app.config.get("SERIAL_BAUDRATE")
-            self.init_app()
+            
+        self.init_app()
     
     # Initialize Application
     def init_app(self):
-        print("running")
-        self._thread = threading.Thread(target=self.__open_serial)
-        self._thread.setDaemon(True)
+
+        self._thread = threading.Thread(target=self.__open_serial,daemon=True)
         self._thread.start()
 
     
@@ -27,24 +33,22 @@ class Serial:
         try:
             self.Ser = self.serial.Serial(self.port, self.baudrate, timeout=self.timeout)
             print("serial is open")
+            
+            while True:
+                if self.Ser.in_waiting > 0:
+                    data = self.Ser.readline().decode().strip()
+                    self.message = data
+                    print(self.message)
+                
+
+                    
         except Exception as e:
             print(e)
             pass
 
     # get serial message
     def get_serial_message(self):
+        print(self.message)
+        return self.message
         
-        while self.Ser is None:
-            # Add some delay to avoid busy-waiting
-            time.sleep(0.1)
-        
-        try:
-            
-            data = self.Ser.readline().decode().strip()
-            print(data)
-            return data
-        except Exception as e:
-            return e
-        
-        
-        
+
